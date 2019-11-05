f4615b17191c38341391e4e33b2dd0760944b66d
Victor Abeledo <abeledovictor@gmail.com>
2019-11-05 10:42:25 -0300

    initial commit

---
+++ app/src/main/java/ar/edu/ips/aus/seminario2/sampleproject/GameView.java
@@ -0,0 +1,241 @@
+package ar.edu.ips.aus.seminario2.sampleproject;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.Rect;
+import android.provider.Settings;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.Toast;
+
+import com.abemart.wroup.client.WroupClient;
+import com.abemart.wroup.common.messages.MessageWrapper;
+import com.abemart.wroup.service.WroupService;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.Vector;
+
+public class GameView extends SurfaceView implements SurfaceHolder.Callback {
+
+    private static final String TAG = GameView.class.getSimpleName();
+    public static final String STATUS_PAUSED = "paused";
+    public static final String STATUS_UPDATING = "updating";
+    private boolean updating = false;
+    private GameAnimationThread thread;
+    private Player player;
+    private Map<String, Player> players = new HashMap<>();
+    private PlayerSprite playerSprites;
+    private int moves = 0;
+    private static final int SERVER_UPDATE_RATIO = 3;
+    private static final int CLIENT_UPDATE_RATIO = 2;
+
+    public GameView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public GameView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    public GameView(Context context){
+        super(context);
+        init();
+    }
+
+    private void init() {
+        getHolder().addCallback(this);
+
+        String id = Settings.Secure.getString(getContext().getContentResolver(),
+                Settings.Secure.ANDROID_ID);
+        player = new Player(id,0.5,0.5);
+        players.put(id, player);
+
+        playerSprites = new PlayerSprite(getResources());
+        player.setOrder(playerSprites.getRandomSpriteNumber());
+
+        thread = new GameAnimationThread(getHolder(), this);
+        setFocusable(true);
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        thread.setRunning(true);
+        thread.start();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        boolean retry = true;
+        while (retry){
+            try {
+                thread.setRunning(false);
+                thread.join();
+            } catch (InterruptedException e) {
+                Log.d(TAG, "Error " + e.getMessage());
+            }
+            retry = false;
+        }
+    }
+
+    // update game world
+    public void update(long delta) {
+        if (this.updating) {
+            MazeBoard board = GameApp.getInstance().getMazeBoard();
+            // update only actual player
+            player.move(board, delta);
+            this.moves++;
+
+            // if we are server send all players data
+            if (GameApp.getInstance().isGameServer()) {
+                if (this.moves % SERVER_UPDATE_RATIO == 0) {
+                    WroupService server = GameApp.getInstance().getServer();
+                    MessageWrapper message = new MessageWrapper();
+                    Gson json = new Gson();
+                    Message<Player[]> data = new Message<Player[]>(Message.MessageType.PLAYER_DATA,
+                            players.values().toArray(new Player[]{}));
+                    String msg = json.toJson(data);
+                    message.setMessage(msg);
+                    message.setMessageType(MessageWrapper.MessageType.NORMAL);
+                    server.sendMessageToAllClients(message);
+                }
+            } else {
+                // if we are client send player data
+                if (this.moves % CLIENT_UPDATE_RATIO == 0) {
+                    WroupClient client = GameApp.getInstance().getClient();
+                    MessageWrapper message = new MessageWrapper();
+                    Gson json = new Gson();
+                    Message<Player[]> data = new Message<Player[]>(Message.MessageType.PLAYER_DATA,
+                            new Player[]{player});
+                    String msg = json.toJson(data);
+                    message.setMessage(msg);
+                    message.setMessageType(MessageWrapper.MessageType.NORMAL);
+                    client.sendMessageToServer(message);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void draw(Canvas canvas){
+        super.draw(canvas);
+        if (canvas != null) {
+            MazeBoard board = GameApp.getInstance().getMazeBoard();
+            canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
+            if (board != null) {
+                for (Player p : this.players.values()) {
+                    Rect srcRect = playerSprites.getSourceRectangle(this, board, p, p.getOrder());
+                    Rect dstRect = playerSprites.getDestinationRectangle(this, board, p);
+                    Log.d("MAZE: ", String.format("src rect: %s - dst rect: %s", srcRect.toShortString(), dstRect.toShortString()));
+                    canvas.drawBitmap(playerSprites.getSprites(), srcRect, dstRect, null);
+                }
+            }
+        }
+    }
+
+    public void updatePlayerData(String message) {
+        Gson gson = new Gson();
+        Message<Player[]> playerData = gson.fromJson(message,
+                new TypeToken<Message<Player[]>>(){}.getType());
+        for (Player pd:playerData.getPayload()) {
+            if (!player.getID().equals(pd.getID())) {
+                Player p = players.get(pd.getID());
+                if (p == null) {
+                    p = new Player(pd.getID(), pd.getX(), pd.getY());
+                    p.setOrder(pd.getOrder());
+                    players.put(pd.getID(), p);
+                }
+                p.setX(pd.getX());
+                p.setY(pd.getY());
+                p.setXVel(pd.getXVel());
+                p.setYVel(pd.getYVel());
+            }
+        }
+    }
+
+    public void updateStatus(String message) {
+        Gson gson = new Gson();
+        Message<String> gameStatus = gson.fromJson(message,
+                new TypeToken<Message<String>>(){}.getType());
+        if (gameStatus.getType() == Message.MessageType.GAME_STATUS){
+            String data = gameStatus.getPayload();
+            switch (data){
+                case STATUS_PAUSED:
+                    this.updating = false;
+                    Log.d(TAG, "Game paused.");
+                    this.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getContext(), "GAME PAUSED by SERVER", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                    break;
+                case STATUS_UPDATING:
+                    this.updating = true;
+                    Log.d(TAG, "Game updating.");
+                    this.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getContext(), "GAME RESUMED by SERVER", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    public void toggleStatus() {
+        if (GameApp.getInstance().isGameServer()) {
+            String status = null;
+            if (this.updating) {
+                this.updating = false;
+                status = STATUS_PAUSED;
+                this.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getContext(), "GAME PAUSED", Toast.LENGTH_LONG).show();
+                    }
+                });
+            } else {
+                this.updating = true;
+                status = STATUS_UPDATING;
+                this.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getContext(), "GAME RESUMED", Toast.LENGTH_LONG).show();
+                    }
+                });            }
+            WroupService server = GameApp.getInstance().getServer();
+            MessageWrapper message = new MessageWrapper();
+            Gson json = new Gson();
+            Message<String> data = new Message<String>(Message.MessageType.GAME_STATUS,
+                    status);
+            String msg = json.toJson(data);
+            message.setMessage(msg);
+            message.setMessageType(MessageWrapper.MessageType.NORMAL);
+            server.sendMessageToAllClients(message);
+        }
+    }
+}
---
+++ app/src/main/java/ar/edu/ips/aus/seminario2/sampleproject/MazeBoardActivity.java
@@ -0,0 +1,242 @@
+package ar.edu.ips.aus.seminario2.sampleproject;
+
+import android.graphics.PixelFormat;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.Toast;
+
+import com.abemart.wroup.client.WroupClient;
+import com.abemart.wroup.common.WroupDevice;
+import com.abemart.wroup.common.listeners.ClientConnectedListener;
+import com.abemart.wroup.common.listeners.ClientDisconnectedListener;
+import com.abemart.wroup.common.listeners.DataReceivedListener;
+import com.abemart.wroup.common.messages.MessageWrapper;
+import com.abemart.wroup.service.WroupService;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static ar.edu.ips.aus.seminario2.sampleproject.Message.MessageType.PLAYER_DATA;
+
+public class MazeBoardActivity extends AppCompatActivity
+        implements View.OnClickListener, DataReceivedListener,
+        ClientConnectedListener, ClientDisconnectedListener {
+
+    public static final String EXTRA_SERVER_NAME = "SERVER_NAME";
+    public static final String EXTRA_IS_SERVER = "IS_SERVER";
+    private static final String TAG = MazeBoardActivity.class.getSimpleName();
+    private static final int MAX_DEVICES = 3;
+
+    private Button buttonUp, buttonDown, buttonLeft, buttonRight, buttonPause;
+
+    ImageView[] imageViews = null;
+
+    private GameView mazeView;
+    private final HashMap<String, WroupDevice> devices = new HashMap<String, WroupDevice>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.maze);
+
+        buttonUp = findViewById(R.id.buttonUp);
+        buttonDown = findViewById(R.id.buttonDown);
+        buttonLeft = findViewById(R.id.buttonLeft);
+        buttonRight = findViewById(R.id.buttonRight);
+        buttonPause = findViewById(R.id.buttonPause);
+
+        buttonUp.setOnClickListener(this);
+        buttonDown.setOnClickListener(this);
+        buttonLeft.setOnClickListener(this);
+        buttonRight.setOnClickListener(this);
+        buttonPause.setOnClickListener(this);
+
+        mazeView = (GameView)findViewById(R.id.gameView);
+        mazeView.getHolder().setFormat(PixelFormat.TRANSLUCENT);
+        mazeView.setZOrderMediaOverlay(true);
+        mazeView.setZOrderOnTop(true);
+
+        GameApp.getInstance().setServerName(getIntent().getStringExtra(this.EXTRA_SERVER_NAME));
+        GameApp.getInstance().setGameServer(getIntent().getBooleanExtra(this.EXTRA_IS_SERVER, false));
+        if (GameApp.getInstance().isGameServer()){
+            WroupService server = WroupService.getInstance(this);
+            server.setDataReceivedListener(this);
+            server.setClientDisconnectedListener(this);
+            server.setClientConnectedListener(this);
+            GameApp.getInstance().setServer(server);
+        } else {
+            WroupClient client = WroupClient.getInstance(this);
+            client.setDataReceivedListener(this);
+            client.setClientDisconnectedListener(this);
+            client.setClientConnectedListener(this);
+            GameApp.getInstance().setClient(client);
+        }
+
+        MazeBoard board = MazeBoard.from("asdasd");
+        GameApp.getInstance().setMazeBoard(board);
+        setupMazeBoard(board);
+    }
+
+    private void setupMazeBoard(MazeBoard board) {
+
+        int height = board.getVerticalTileCount();
+        int width = board.getHorizontalTileCount();
+
+        imageViews = new ImageView[width * height];
+
+        int resId = 0;
+
+        TableLayout table = findViewById(R.id.mazeBoard);
+        for (int i=0; i<height; i++){
+            TableRow row = new TableRow(this);
+            TableLayout.LayoutParams rowParams = new TableLayout.LayoutParams();
+            rowParams.width = TableRow.LayoutParams.MATCH_PARENT;
+            rowParams.height = TableRow.LayoutParams.MATCH_PARENT;
+            rowParams.weight = 1;
+            rowParams.gravity = Gravity.CENTER;
+            row.setLayoutParams(rowParams);
+            table.addView(row);
+
+            for (int j=0; j<width; j++){
+                BoardPiece piece = board.getPiece(j, i);
+
+                resId = lookupResource(piece);
+
+                ImageView imageView = new ImageView(this);
+                imageView.setBackgroundResource(resId);
+                TableRow.LayoutParams imageViewParams = new TableRow.LayoutParams();
+                imageViewParams.width = ViewGroup.LayoutParams.MATCH_PARENT;
+                imageViewParams.height = ViewGroup.LayoutParams.MATCH_PARENT;
+                imageViewParams.weight = 1;
+                imageView.setLayoutParams(imageViewParams);
+
+                row.addView(imageView);
+
+                imageViews[(j%board.getHorizontalTileCount())+ board.getVerticalTileCount()*i] = imageView;
+            }
+        }
+        table.invalidate();
+   }
+
+    private int lookupResource(BoardPiece piece) {
+        int iconIndex = 0b1000 * (piece.isOpen(MazeBoard.Direction.WEST)? 1:0) +
+                0b0100 * (piece.isOpen(MazeBoard.Direction.NORTH)? 1:0) +
+                0b0010 * (piece.isOpen(MazeBoard.Direction.EAST)? 1:0) +
+                0b0001 * (piece.isOpen(MazeBoard.Direction.SOUTH)? 1:0);
+
+        int[] iconLookupTable = { 0,
+                R.drawable.m1b,
+                R.drawable.m1r,
+                R.drawable.m2rb,
+                R.drawable.m1t,
+                R.drawable.m2v,
+                R.drawable.m2tr,
+                R.drawable.m3l,
+                R.drawable.m1l,
+                R.drawable.m2bl,
+                R.drawable.m2h,
+                R.drawable.m3t,
+                R.drawable.m2lt,
+                R.drawable.m3r,
+                R.drawable.m3b,
+                R.drawable.m4};
+
+        return iconLookupTable[iconIndex];
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == buttonUp) {
+            mazeView.getPlayer().setNewDirection(MazeBoard.Direction.NORTH);
+        }
+        else if (v == buttonDown) {
+            mazeView.getPlayer().setNewDirection(MazeBoard.Direction.SOUTH);
+        }
+        else if (v == buttonLeft) {
+            mazeView.getPlayer().setNewDirection(MazeBoard.Direction.WEST);
+        }
+        else if (v == buttonRight) {
+            mazeView.getPlayer().setNewDirection(MazeBoard.Direction.EAST);
+        } else if (v == buttonPause) {
+            mazeView.toggleStatus();
+        }
+
+    }
+
+    @Override
+    public void onClientConnected(final WroupDevice wroupDevice) {
+        if (GameApp.getInstance().isGameServer())
+            addToDeviceList(wroupDevice);
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), getString(R.string.device_connected, wroupDevice.getDeviceName()), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public void onClientDisconnected(final WroupDevice wroupDevice) {
+        if (GameApp.getInstance().isGameServer())
+            removeFromDeviceList(wroupDevice);
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), getString(R.string.device_disconnected, wroupDevice.getDeviceName()), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private boolean addToDeviceList(WroupDevice wroupDevice) {
+        if (devices.size() < MAX_DEVICES) {
+            devices.put(wroupDevice.getDeviceMac(), wroupDevice);
+            return true;
+        }
+        return false;
+    }
+
+    private boolean removeFromDeviceList(WroupDevice wroupDevice) {
+        return (devices.remove(wroupDevice.getDeviceMac()) != null);
+    }
+
+    @Override
+    public void onDataReceived(MessageWrapper messageWrapper) {
+        if (!GameApp.getInstance().isGameServer()) {
+            // client may receive different kind of messages from server
+            JsonObject object = JsonParser.parseString(messageWrapper.getMessage()).getAsJsonObject();
+            JsonElement typeElement = object.get("type");
+            switch (Message.MessageType.valueOf(typeElement.getAsString())){
+                case PLAYER_DATA:
+                    mazeView.updatePlayerData(messageWrapper.getMessage());
+                    break;
+                case GAME_DATA:
+                    break;
+                case GAME_STATUS:
+                    mazeView.updateStatus(messageWrapper.getMessage());
+                    break;
+            }
+        } else {
+            // server receives individual player data
+            if (devices.containsKey(messageWrapper.getWroupDevice().getDeviceMac())){
+                mazeView.updatePlayerData(messageWrapper.getMessage());
+            }
+        }
+
+    }
+}
